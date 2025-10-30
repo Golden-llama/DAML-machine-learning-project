21370645e2307bfb3916221277bf920a6369d188
Alysa Yao <alysa.yao@duke.edu>
2025-10-30 01:38:28 -0400

    removed

--- file.py
+++ file.py
@@ -1,2 +1,24 @@
+import pandas as pd
+import string
+import re
+
+df = pd.read_csv('WELFake_Dataset.csv')
+
+print(df.columns)
+
+#removing punctuations
+
+def remove_punctuation(text):
+    if isinstance(text, str):
+        return re.sub(f"[{re.escape(string.punctuation)}]", "", text)
+    return text
+
+df["text_no_punct"] = df["text"].apply(remove_punctuation)
+
+df.to_csv("nopunct.csv", index =False)
+
+print(df.head())
+
+
 
 
