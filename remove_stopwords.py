051abac65ad3b2e6f6c066acbe95611378d94731
Alysa Yao <alysa.yao@duke.edu>
2025-10-30 19:51:39 -0400

    added ipynb

--- remove_stopwords.py
+++ remove_stopwords.py
@@ -5,5 +5,18 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
 df = pd.read_csv('nopunct.csv')
+stop_words = set(stopwords.words('english'))
+
+def remove_stopwords(text):
+    if isinstance(text, str):
+        words = word_tokenize(text)
+        filtered = [word for word in words if word.lower() not in stop_words]
+        return " ".join(filtered)
+    return text
+df['text'] = df['text_no_punct'].apply(remove_stopwords)
+
+df['title'] = df['title_no_punct'].apply(remove_stopwords)
+
