4d0c329f299d9b491a99e4df9de523ac7f9fa51a
Alysa Yao <alysa.yao@duke.edu>
2025-11-11 01:20:16 -0500

    lemmatized

--- clean-text/lemmatization.py
+++ clean-text/lemmatization.py
@@ -1,8 +1,25 @@
 import pandas as pd
 from nltk.stem import WordNetLemmatizer
-import re
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
+df = pd.read_csv('nopunctANDstopwords.csv')
 
-def clean_lemmatize(text):
-    
+df[['title','text']] = df[['title','text']].replace(',',' ',regex=True)
+
+def lemmatize(text):
+    if isinstance(text, str):
+        tokens = word_tokenize(text)
+        lemmas = [lemmatizer.lemmatize(word) for word in tokens]
+        return ' '.join(lemmas)
+    else:
+        return text
+
+df[['title','text']] = df[['title','text']].applymap(lemmatize)
+
+df.to_csv('lemmatized.csv', index=False)
+
+
